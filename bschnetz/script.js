6c61d24dfa9e4f3fd4a8a0444b4a06eb06b8e857
Brandon <134080428+Brandon-Sch@users.noreply.github.com>
2024-10-26 23:59:41 -0400

    Tutorial Color Bar: Done

---
+++ bschnetz/script.js
@@ -0,0 +1,25 @@
+/*
+Template format for a listener
+
+const <Button> = document.getElementById('id');
+<Button>.addEventListener('input', function() {
+});
+*/
+
+const colorDisplay = document.getElementById('colorBox');
+const changeColorButton = document.getElementById('changeColorBtn');
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+changeColorButton.addEventListener('click', function() {
+  const newColor = getRandomColor();
+  colorDisplay.style.backgroundColor = newColor;
+  colorDisplay.textContent = newColor;
+});
